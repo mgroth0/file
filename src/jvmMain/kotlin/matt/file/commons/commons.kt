378cff6f15f395463020a22e271beea2777e678f
Matthew Groth <matthewgroth@Matthews-MacBook-Pro.local>
2022-07-31 07:28:53 -0400

    autocommit

--- src/jvmMain/kotlin/matt/file/commons/commons.kt
+++ src/jvmMain/kotlin/matt/file/commons/commons.kt
@@ -24,6 +24,8 @@ val ICON_FOLDER by lazy { REGISTERED_FOLDER["icon"] }
 val BIN_FOLDER by lazy { REGISTERED_FOLDER + "bin" }
 val BIN_BIN by lazy { BIN_FOLDER + "bin" }
 val BIN_BIN_BIN by lazy { BIN_BIN + "bin" }
+val BIN_BIN_BIN_BIN by lazy { BIN_BIN_BIN + "bin" }
+val DIST_FOLDER by lazy { REGISTERED_FOLDER + "dist" }
 val BIN_NATIVE_FOLDER by lazy { BIN_FOLDER + "native" }
 val BIN_JAR_FOLDER by lazy { BIN_FOLDER + "jar" }
 
