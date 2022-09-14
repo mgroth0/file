c8c07940d23f3136f43e138a9a4239e5fd4ba719
mgroth0 <mgroth49@gmail.com>
2022-09-14 19:45:34 -0400

    autocommit

--- src/commonMain/kotlin/matt/file/icongen/icongen.kt
+++ src/commonMain/kotlin/matt/file/icongen/icongen.kt
@@ -2,5 +2,5 @@
 package matt.file.icongen
 
 enum class Icon {
-    slides, finder, icon, code, mail, lit, chunk, web, comp, clock, pencil, clock_b, loop, slide, folder
+    slides, finder, icon, code, mail, intellij, lit, chunk, web, comp, clock, pencil, clock_b, loop, slide, folder
 }
