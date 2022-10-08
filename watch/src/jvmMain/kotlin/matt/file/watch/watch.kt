a6ce479297027454919687fb1ff625f75e10f775
mgroth0 <mgroth49@gmail.com>
2022-10-07 21:23:06 -0400

    autocommit

--- watch/src/jvmMain/kotlin/matt/file/watch/watch.kt
+++ watch/src/jvmMain/kotlin/matt/file/watch/watch.kt
@@ -1,17 +1,22 @@
 package matt.file.watch
 
 import matt.file.MFile
-import matt.obs.prop.BindableProperty
-import kotlin.concurrent.thread
+import matt.obs.watch.watchProp
+import kotlin.time.Duration
 
-fun MFile.createRecursiveLastModifiedProp(checkFreqMillis: Long): BindableProperty<Long> {
-  val prop = BindableProperty(recursiveLastModified())
-  thread(isDaemon = true) {
-	while (true) {
-	  val m = recursiveLastModified()
-	  if (m != prop.value) prop.value = m
-	  Thread.sleep(checkFreqMillis)
-	}
-  }
-  return prop
+fun MFile.createRecursiveLastModifiedProp(checkInterval: Duration) = watchProp(checkInterval) {
+  recursiveLastModified()
+}
+
+fun MFile.createFileSizeProp(checkInterval: Duration) = watchProp(checkInterval) {
+  takeIf { exists() }?.size()
+}
+
+fun MFile.createRecursiveFileSizeProp(checkInterval: Duration) = watchProp(checkInterval) {
+  takeIf { exists() }?.recursiveSize()
+}
+
+
+fun MFile.createFileExistsProp(checkInterval: Duration) = watchProp(checkInterval) {
+  exists()
 }
