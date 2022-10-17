e8cdd2628d9d54932644dc6197d0cc704df6cdca
mgroth0 <mgroth49@gmail.com>
2022-10-17 17:04:52 -0400

    autocommit

--- src/commonMain/kotlin/matt/file/gen/icon/icon.kt
+++ src/commonMain/kotlin/matt/file/gen/icon/icon.kt
@@ -1,6 +1,4 @@
  
 package matt.file.gen.icon
 
-enum class Icon {
-    slides, finder, icon, code, mail, intellij, lit, chunk, web, comp, clock, pencil, clock_b, loop, slide, folder
-}
+enum class Icon()
