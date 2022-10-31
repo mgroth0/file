b1e5b56d838a1f294af5dd5ea2480b07d2c74906
mgroth0 <mgroth49@gmail.com>
2022-10-30 21:41:19 -0400

    autocommit

--- src/jvmMain/kotlin/matt/file/file.kt
+++ src/jvmMain/kotlin/matt/file/file.kt
@@ -42,7 +42,6 @@ actual sealed class MFile actual constructor(actual val userPath: String): File(
 																		   WritableText,
 																		   IDFile {
 
-
   actual override val filePath: String get() = super.getPath()
   actual override val cpath: String = path
   val userFile = File(this.cpath)
