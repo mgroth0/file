62bdf0454dfc1c9b2fd83883bb84d6ca0b52d3ee
mgroth0 <mgroth49@gmail.com>
2022-10-31 21:03:43 -0400

    autocommit

--- src/jvmMain/kotlin/matt/file/file.kt
+++ src/jvmMain/kotlin/matt/file/file.kt
@@ -12,12 +12,12 @@ import matt.file.thismachine.thisMachine
 import matt.lang.NOT_IMPLEMENTED
 import matt.lang.userHome
 import matt.log.NOPLogger
-import matt.log.reporter.Reporter
 import matt.log.warn.warn
 import matt.model.byte.ByteSize
 import matt.model.file.IDFile
 import matt.model.file.IDFolder
 import matt.model.ok.JavaIoFileIsOk
+import matt.model.report.Reporter
 import matt.model.stream.Streamable
 import matt.model.text.WritableText
 import matt.prim.str.lower
