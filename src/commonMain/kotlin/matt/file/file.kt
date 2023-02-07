e76c859d518ef79fd685779e4478a08c37a855a9
mgroth0 <mgroth49@gmail.com>
2023-02-06 22:27:39 -0500

    autocommit

--- src/commonMain/kotlin/matt/file/file.kt
+++ src/commonMain/kotlin/matt/file/file.kt
@@ -92,7 +92,44 @@ expect sealed class MFile(userPath: String): CommonFile, MightExistAndWritableTe
 }
 
 
-internal annotation class Extensions(vararg val exts: String)
+fun fileClassForExtension(extension: FileExtension): KClass<out MFile> {
+  return when (extension.afterDot) {
+	"kt"           -> KotlinFile::class
+	"py"           -> PythonFile::class
+	"java"         -> JavaFile::class
+	"groovy"       -> GroovyFile::class
+	"sh"           -> ShellFileImpl::class
+	"zshrc", "zsh" -> ZshFile::class
+	"scpt"         -> BinaryApplescriptFile::class
+	"applescript"  -> ApplescriptFile::class
+	"zip"          -> ZipFile::class
+	"jar"          -> JarFile::class
+	"kexe"         -> KExeFile::class
+	"exe"          -> ExeFile::class
+	"dmg"          -> DmgFile::class
+	"json"         -> JsonFile::class
+	"cbor"         -> CborFile::class
+	"xml"          -> XMLFile::class
+	"html"         -> HTMLFile::class
+	"md"           -> MarkDownFile::class
+	"png"          -> PngFile::class
+	"jpg", "jpeg"  -> JpgFile::class
+	"tif", "tiff"  -> TiffFile::class
+	"svg"          -> SvgFile::class
+	"icns"         -> ICNSFile::class
+	"ico"          -> ICOFile::class
+	"mp3"          -> MP3File::class
+	"mp4"          -> MP4File::class
+	"pdf"          -> PdfFile::class
+	"properties"   -> PropsFile::class
+	"yaml", "yml"  -> YamlFile::class
+	"toml"         -> TomlFile::class
+	"log"          -> LogFile::class
+	"txt"          -> TxtFile::class
+	"DS_Store"     -> DSStoreFile::class
+	else           -> UnknownFile::class
+  }
+}
 
 
 class UnknownFile(userPath: String): MFile(userPath)
@@ -111,7 +148,7 @@ sealed class CodeFile(userPath: String): MFile(userPath)
 
 val String.kt get() = KotlinFile("$this.kt")
 
-@Extensions("kt") class KotlinFile(userPath: String): CodeFile(userPath) {
+class KotlinFile(userPath: String): CodeFile(userPath) {
   companion object {
 	const val FILE_ANNO_LINE_MARKER = "@file:"
   }
@@ -119,107 +156,111 @@ val String.kt get() = KotlinFile("$this.kt")
 
 val String.py get() = PythonFile("$this.py")
 
-@Extensions("py") class PythonFile(userPath: String): CodeFile(userPath)
-@Extensions("java") class JavaFile(userPath: String): CodeFile(userPath)
-@Extensions("groovy") class GroovyFile(userPath: String): CodeFile(userPath)
+class PythonFile(userPath: String): CodeFile(userPath)
+class JavaFile(userPath: String): CodeFile(userPath)
+class GroovyFile(userPath: String): CodeFile(userPath)
 interface ShellFile: CommonFile
 
 val String.sh get() = ShellFileImpl("$this.sh")
 
-@Extensions("sh") class ShellFileImpl(userPath: String): CodeFile(userPath), ShellFile
-@Extensions("zshrc", "zsh") class ZshFile(userPath: String): CodeFile(userPath), ShellFile
+class ShellFileImpl(userPath: String): CodeFile(userPath), ShellFile
+class ZshFile(userPath: String): CodeFile(userPath), ShellFile
 
 val String.scpt get() = BinaryApplescriptFile("$this.scpt")
 
-@Extensions("scpt") class BinaryApplescriptFile(userPath: String): ExecutableFile(userPath)
+class BinaryApplescriptFile(userPath: String): ExecutableFile(userPath)
 
 val String.applescript get() = ApplescriptFile("$this.applescript")
 
-@Extensions("applescript") class ApplescriptFile(userPath: String): CodeFile(userPath)
+class ApplescriptFile(userPath: String): CodeFile(userPath)
 
 interface ArchiveFile: CommonFile
 
 sealed class BaseZip(userPath: String): MFile(userPath), ArchiveFile
 
-@Extensions("zip") class ZipFile(userPath: String): BaseZip(userPath)
+class ZipFile(userPath: String): BaseZip(userPath)
 
 val String.jar get() = JarFile("$this.jar")
 
-@Extensions("jar") class JarFile(userPath: String): BaseZip(userPath)
+class JarFile(userPath: String): BaseZip(userPath)
 
 sealed class ExecutableFile(userPath: String): MFile(userPath)
 
 val String.kexe get() = KExeFile("$this.kexe")
 
-@Extensions("kexe") class KExeFile(userPath: String): ExecutableFile(userPath)
-@Extensions("exe") class ExeFile(userPath: String): ExecutableFile(userPath)
+class KExeFile(userPath: String): ExecutableFile(userPath)
+class ExeFile(userPath: String): ExecutableFile(userPath)
 
 
 sealed class DiskImageFile(userPath: String): MFile(userPath)
-@Extensions("dmg") class DmgFile(userPath: String): DiskImageFile(userPath)
+class DmgFile(userPath: String): DiskImageFile(userPath)
 
 
 sealed class DataFile(
-  userPath: String,
-  val binary: Boolean
+  userPath: String, val binary: Boolean
 ): MFile(userPath) //sealed class HumanReadableDataFile(userPath: String): DataFile(userPath) //sealed class BinaryDataFile(userPath: String): DataFile(userPath)
 
 
 val String.json get() = JsonFile("$this.json")
 
-@Extensions("json") class JsonFile(userPath: String): DataFile(userPath, binary = false)
+class JsonFile(userPath: String): DataFile(userPath, binary = false)
 
 val String.cbor get() = CborFile("$this.cbor")
 
-@Extensions("cbor") class CborFile(userPath: String): DataFile(userPath, binary = true)
+class CborFile(userPath: String): DataFile(userPath, binary = true)
 
 
 sealed interface MarkupLanguageFile: CommonFile
-@Extensions("xml") class XMLFile(userPath: String): DataFile(userPath, binary = false), MarkupLanguageFile
-@Extensions("html") class HTMLFile(userPath: String): MFile(userPath), MarkupLanguageFile
+class XMLFile(userPath: String): DataFile(userPath, binary = false), MarkupLanguageFile
+=class HTMLFile(userPath: String): MFile(userPath), MarkupLanguageFile
 
 val String.md get() = MarkDownFile("$this.md")
 
-@Extensions("md") class MarkDownFile(userPath: String): MFile(userPath), MarkupLanguageFile
+class MarkDownFile(userPath: String): MFile(userPath), MarkupLanguageFile
 
 
 sealed class ImageFile(userPath: String, val raster: Boolean): MFile(userPath)
 
 val String.png get() = PngFile("$this.png")
-@Extensions("png") class PngFile(userPath: String): ImageFile(userPath, raster = true)
-@Extensions("jpg,jpeg") class JpgFile(userPath: String): ImageFile(userPath, raster = true)
-@Extensions("tif", "tiff") class TiffFile(userPath: String): ImageFile(userPath, raster = true)
+
+class PngFile(userPath: String): ImageFile(userPath, raster = true)
+class JpgFile(userPath: String): ImageFile(userPath, raster = true)
+class TiffFile(userPath: String): ImageFile(userPath, raster = true)
 
 val String.svg get() = SvgFile("$this.svg")
-@Extensions("svg") class SvgFile(userPath: String): ImageFile(userPath, raster = false)
+
+class SvgFile(userPath: String): ImageFile(userPath, raster = false)
 
 val String.icns get() = ICNSFile("$this.icns")
-@Extensions("icns") class ICNSFile(userPath: String): ImageFile(userPath, raster = true)
+
+
+class ICNSFile(userPath: String): ImageFile(userPath, raster = true)
 
 val String.ico get() = ICOFile("$this.ico")
-@Extensions("ico") class ICOFile(userPath: String): ImageFile(userPath, raster = true)
+
+class ICOFile(userPath: String): ImageFile(userPath, raster = true)
 
 
-@Extensions("mp3") class MP3File(userPath: String): MFile(userPath)
+class MP3File(userPath: String): MFile(userPath)
 
 val String.mp4 get() = MP4File("$this.mp4")
 
-@Extensions("mp4") class MP4File(userPath: String): MFile(userPath)
+class MP4File(userPath: String): MFile(userPath)
 
-@Extensions("pdf") class PdfFile(userPath: String): MFile(userPath)
+class PdfFile(userPath: String): MFile(userPath)
 
-@Extensions("properties") class PropsFile(userPath: String): DataFile(userPath, binary = false)
+class PropsFile(userPath: String): DataFile(userPath, binary = false)
 
-@Extensions("yaml", "yml") class YamlFile(userPath: String): DataFile(userPath, binary = false)
-@Extensions("toml") class TomlFile(userPath: String): DataFile(userPath, binary = false)
+class YamlFile(userPath: String): DataFile(userPath, binary = false)
+class TomlFile(userPath: String): DataFile(userPath, binary = false)
 
 val String.log get() = LogFile("$this.log")
 
-@Extensions("log") class LogFile(userPath: String): MFile(userPath)
+class LogFile(userPath: String): MFile(userPath)
 
 val String.txt get() = TxtFile("$this.txt")
 
-@Extensions("txt") class TxtFile(userPath: String): MFile(userPath)
-@Extensions("DS_Store") class DSStoreFile(userPath: String): DataFile(userPath, binary = false)
+class TxtFile(userPath: String): MFile(userPath)
+class DSStoreFile(userPath: String): DataFile(userPath, binary = false)
 
 
--- src/jvmMain/kotlin/matt/file/construct/construct.kt
+++ src/jvmMain/kotlin/matt/file/construct/construct.kt
@@ -3,18 +3,17 @@
 package matt.file.construct
 
 import matt.collect.map.dmap.withStoringDefault
-import matt.collect.itr.recurse.recurse
 import matt.collect.map.lazyMap
-import matt.file.Extensions
+import matt.file.FileExtension
 import matt.file.Folder
 import matt.file.MFile
-import matt.file.UnknownFile
+import matt.file.fileClassForExtension
 import matt.model.code.ok.JavaIoFileIsOk
 import java.io.File
+import java.lang.reflect.Constructor
 import java.net.URI
 import java.nio.file.Path
 import kotlin.reflect.KClass
-import kotlin.reflect.KFunction
 
 fun Path.toMFile() = toFile().toMFile()
 fun File.toMFile(cls: KClass<out MFile>? = null) = mFile(this, cls = cls)
@@ -30,10 +29,12 @@ fun mFile(uri: URI) = mFile(File(uri))
 actual fun mFile(userPath: String, cls: KClass<out MFile>?): MFile {
   if (cls != null && cls != MFile::class) {
 	val constructor = constructorsByCls[cls]
-	return constructor.call(userPath)
+	/*return constructor.call(userPath)*/
+	return constructor.newInstance(userPath) as MFile
   }
   val f = File(userPath)
-  return constructors[f.extension].call(userPath)
+  return constructors[f.extension].newInstance(userPath) as MFile
+  /*.call(userPath)*/
 }
 
 fun mFolder(userPath: String): Folder {
@@ -42,18 +43,28 @@ fun mFolder(userPath: String): Folder {
 
 
 private val fileTypes by lazy {
+
+
   mutableMapOf<String, KClass<out MFile>>().withStoringDefault { extension ->
-	MFile::class.sealedSubclasses.flatMap { it.recurse { it.sealedSubclasses } }.firstOrNull {
-	  val b = it.annotations.filterIsInstance<Extensions>().firstOrNull()?.exts?.let { extension in it } ?: false
-	  b
-	} ?: UnknownFile::class
+
+	fileClassForExtension(FileExtension(extension))
+
+	/*	MFile::class.sealedSubclasses.flatMap { it.recurse { it.sealedSubclasses } }.firstOrNull {
+		  val b = it.annotations.filterIsInstance<Extensions>().firstOrNull()?.exts?.let { extension in it } ?: false
+		  b
+		} ?: UnknownFile::class*/
+
+
   }
+
+
 }
-private val constructors = lazyMap<String, KFunction<MFile>> {
+private val constructors = lazyMap<String, Constructor<*>> {
   /*surprisingly getting constructors is expensive so this could have a huge performance benefit and even solve some bugs maybe*/
   constructorsByCls[fileTypes[it]]
 }
-private val constructorsByCls = lazyMap<KClass<out MFile>, KFunction<MFile>> {
+private val constructorsByCls = lazyMap<KClass<out MFile>, Constructor<*>> {
   /*surprisingly getting constructors is expensive so this could have a huge performance benefit and even solve some bugs maybe*/
-  it.constructors.first()
+  it.java.constructors.first()
+  /*it.constructors.first()*/
 }
