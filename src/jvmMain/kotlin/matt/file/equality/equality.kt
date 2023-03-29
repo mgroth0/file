74e9f4ceb25821a62bd204e2ae81ecfce39f31f6
mgroth0 <mgroth49@gmail.com>
2023-03-29 03:31:42 -0400

    autocommit

--- src/jvmMain/kotlin/matt/file/equality/equality.kt
+++ src/jvmMain/kotlin/matt/file/equality/equality.kt
@@ -36,34 +36,41 @@ infix fun MFile.hasIdenticalDataTo(other: MFile): Boolean {
 fun Folder.isRecursivelyIdenticalTo(
     other: Folder,
     ignoreDSStore: Boolean = true
-): Boolean {
-    if (name != other.name) return false
+): Boolean = firstRecursiveDiff(other,ignoreDSStore) != null
+
+fun Folder.firstRecursiveDiff(
+    other: Folder,
+    ignoreDSStore: Boolean = true
+): String? {
+    if (name != other.name) return "name $name is different from ${other.name}"
 
     fun predicate(file: MFile) = !ignoreDSStore || file.name != DS_STORE
 
     val files = this.listFiles()!!.filter(::predicate)
     val otherFiles = other.listFiles()!!.filter(::predicate)
 
-    if (files.size != otherFiles.size) return false
+    if (files.size != otherFiles.size) return "${this.path}: size ${files.size} is different from ${otherFiles.size}"
 
 
     files.forEach { file ->
-        val otherFile = otherFiles.firstOrNull { it.name == file.name } ?: return false
+        val otherFile = otherFiles.firstOrNull { it.name == file.name } ?: return "${this.path}: otherFiles has no ${file.name}"
         if (file.isDir()) {
-            if (!otherFile.isDir()) return false
-            if (!Folder(file.absolutePath).isRecursivelyIdenticalTo(
-                    Folder(otherFile.absolutePath),
-                    ignoreDSStore = ignoreDSStore
-                )
-            ) return false
+            if (!otherFile.isDir()) return "${this.path}: $otherFile is not a dir"
+            val rResult = Folder(file.absolutePath).firstRecursiveDiff(
+                Folder(otherFile.absolutePath),
+                ignoreDSStore = ignoreDSStore
+            )
+            if (rResult!=null) {
+                return rResult
+            }
         } else {
-            if (otherFile.isDir()) return false
-            if (!file.hasIdenticalDataTo(otherFile)) return false
+            if (otherFile.isDir()) return "${this.path}: $otherFile is a dir"
+            if (!file.hasIdenticalDataTo(otherFile)) return "${this.path}: $otherFile has different data"
         }
     }
 
 
 
 
-    return true
+    return null
 }
