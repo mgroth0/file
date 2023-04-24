26d22cbc0e8d9c4fcf9bb2a19dc04e3918855d61
mgroth0 <mgroth49@gmail.com>
2023-04-24 11:59:09 -0400

    changed src/commonMain/kotlin/matt/file/gen/icon/icon.kt

--- src/commonMain/kotlin/matt/file/gen/icon/icon.kt
+++ src/commonMain/kotlin/matt/file/gen/icon/icon.kt
@@ -1,25 +1,25 @@
-
-package matt.file.gen.icon
-
-
-
-enum class Icon() {
-    slides(),
-    finder(),
-    icon(),
-    code(),
-    mail(),
-    gmail(),
-    BRAINSTORMicon(),
-    icon2(),
-    intellij(),
-    lit(),
-    web(),
-    comp(),
-    clock(),
-    pencil(),
-    clock_b(),
-    loop(),
-    slide(),
-    folder(),
+	  
+	  package matt.file.gen.icon
+	  
+	  
+	  
+	   enum class Icon() {
+slides(),
+finder(),
+icon(),
+code(),
+mail(),
+gmail(),
+BRAINSTORMicon(),
+icon2(),
+intellij(),
+lit(),
+web(),
+comp(),
+clock(),
+pencil(),
+clock_b(),
+loop(),
+slide(),
+folder();
 }
